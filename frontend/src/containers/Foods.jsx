a11a1fce6a1c693fb9747aee8143af1eff76d4f1
rioto.nagano <kingdom950927@gmail.com>
2022-11-13 16:10:21 +0900

    itemクリック時に表示されるモーダルの作成

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { LocalMallIcon } from '../components/Icons';
 import { FoodWrapper } from '../components/FoodWrapper';
 import Skeleton from '@material-ui/lab/Skeleton';
+import { FoodOrderDialog } from '../components/FoodOrderDialog';
 
 // reducers
 import {
@@ -121,6 +122,17 @@ export const Foods = ({match}) => {
               )
           }
         </FoodsList>
+        {
+        state.isOpenOrderDialog &&
+          <FoodOrderDialog
+            food={state.selectedFood}
+            isOpen={state.isOpenOrderDialog}
+            onClose={() => setState({
+              ...state,
+              isOpenOrderDialog: false,
+            })}
+          />
+      }
     </>
   )
 }
