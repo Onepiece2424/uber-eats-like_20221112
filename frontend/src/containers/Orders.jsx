4e7b7c14373f228856529c89bb6ed0dd4c659b0a
rioto.nagano <kingdom950927@gmail.com>
2022-11-13 18:28:44 +0900

    fetchLineFoods()からAPIを呼ぶ

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,16 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer  } from 'react';
 import { fetchLineFoods } from '../apis/line_foods';
+
+// reducers
+import {
+  initialState,
+  lineFoodsActionTyps,
+  lineFoodsReducer,
+} from '../reducers/lineFoods';
 
 export const Orders = () => {
 
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
   useEffect(() => {
+    dispatch({ type: lineFoodsActionTyps.FETCHING });
     fetchLineFoods()
       .then((data) =>
-        console.log(data)
+        dispatch({
+          type: lineFoodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
       )
       .catch((e) => console.error(e));
   }, []);
-  
+
   return (
     <>
       注文画面
