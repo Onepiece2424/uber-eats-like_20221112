9e118728e68a9839199c5999ea54dda88a84e9c3
rioto.nagano <kingdom950927@gmail.com>
2022-11-13 18:19:33 +0900

    fetchLineFoods()関数実行後のdataをconsoleに表示

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,9 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { useEffect } from 'react';
+import { fetchLineFoods } from '../apis/line_foods';
 
 export const Orders = () => {
+
+  useEffect(() => {
+    fetchLineFoods()
+      .then((data) =>
+        console.log(data)
+      )
+      .catch((e) => console.error(e));
+  }, []);
+  
   return (
-    <Fragment>
+    <>
       注文画面
-    </Fragment>
+    </>
   )
 }
