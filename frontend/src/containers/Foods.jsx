c50c441275230cdd9a7c6a705f593c2dfbb659ca
rioto.nagano <kingdom950927@gmail.com>
2022-11-13 15:02:06 +0900

    スタイルの適用

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,4 +1,11 @@
 import React, { useEffect, useReducer } from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+// components
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 // reducers
 import {
@@ -10,8 +17,42 @@ import {
 // apis
 import { fetchFoods } from '../apis/foods';
 
+// images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
 // constants
+import { COLORS } from '../style_constants';
 import { REQUEST_STATE } from '../constants';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
 
 export const Foods = ({match}) => {
 
@@ -28,24 +69,44 @@ export const Foods = ({match}) => {
           }
         });
       })
-  }, [])
+  }, [match])
 
   return (
     <>
-      {
-        foodsState.fetchState === REQUEST_STATE.LOADING ?
-          <>
-            <p>
-              ロード中...
-            </p>
-          </>
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </>
         :
-          foodsState.foodsList.map(food =>
-            <div key={food.id}>
-              {food.name}
-            </div>
-          )
-      }
+        foodsState.foodsList.map(food =>
+          <ItemWrapper key={food.id}>
+            <FoodWrapper
+              food={food}
+              onClickFoodWrapper={(food) => console.log(food)}
+              imageUrl={FoodImage}
+            />
+          </ItemWrapper>
+              )
+          }
+        </FoodsList>
     </>
   )
 }
