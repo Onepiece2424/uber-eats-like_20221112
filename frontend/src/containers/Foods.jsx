75068b79ba1ca1fe64e0d1d5785bf7702a734d9f
rioto.nagano <kingdom950927@gmail.com>
2022-11-13 13:23:32 +0900

    food一覧ページでロード中のテキスト表示

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,20 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+// reducers
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from '../reducers/foods';
 
 // apis
 import { fetchFoods } from '../apis/foods';
 
+// constants
+import { REQUEST_STATE } from '../constants';
+
 export const Foods = ({match}) => {
 
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
   useEffect(() => {
+    dispatch({ type: foodsActionTyps.FETCHING });
     fetchFoods(match.params.restaurantsId)
-    .then((data) =>
-      console.log(data)
-    )
+      .then((data) => {
+        dispatch({
+          type: foodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
   }, [])
 
   return (
     <>
-      フード一覧
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <>
+            <p>
+              ロード中...
+            </p>
+          </>
+        :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
     </>
   )
 }
